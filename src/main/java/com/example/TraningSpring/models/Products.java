858963b3a2f71aaa6c300497b13b3f13326d873d
Mr.D <danil36rusdanil@mail.ru>
2024-03-14 14:27:46 +0300

    Создали базу данных, добавил данные для подключенния к базе данных, обновил класс Products

--- src/main/java/com/example/TraningSpring/models/Products.java
+++ src/main/java/com/example/TraningSpring/models/Products.java
@@ -2,17 +2,30 @@ package com.example.TraningSpring.models;
 
 
 
+import jakarta.persistence.*;
 import lombok.*;
 
-
+@Entity
+@Table(name = "products")
 @Data //за нас создаёт пустой конструктор класса, создаёт get and set, переписывает методы по типу hascode()
 @AllArgsConstructor //создаёт констурктор со всеми полями
+@NoArgsConstructor // созадёт пустой конструктор
 public class Products {
+
+
+    @Id // присваивает значение праймори key
+    @GeneratedValue(strategy =  GenerationType.AUTO) // выполняет функцию Surell
+    @Column(name = "id")// создаёт колонку
     private Long id;
+    @Column(name = "name")// создаёт колонку
     private String name;
+    @Column(name = "description" , columnDefinition = "text")// создаёт колонку
     private String description;
+    @Column(name = "price")// создаёт колонку
     private int price;
+    @Column(name = "city")// создаёт колонку
     private String city;
+    @Column(name = "author")// создаёт колонку
     private String author;
 
 }
